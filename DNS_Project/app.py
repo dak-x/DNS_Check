979a2cf37392a35601f02eefe1ac4286955faf0d
Samarth Singh <45368705+sam6134@users.noreply.github.com>
2020-10-03 23:38:07 +0530

    Add files via upload

--- DNS_Project/app.py
+++ DNS_Project/app.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template, Response, flash, request, send_file
-from form import DNSForm
+from form import DNSForm, RevForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '21a00ee024ebe902cf1848208f5c1a29'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
-
 @app.route('/')
 def index():
     return render_template('home.html')
-
-
 @app.route("/dnslookup", methods=["GET", "POST"])
 def dnslookup():
     form = DNSForm()
     if form.validate_on_submit():
-        return "dns return statements"
+        return render_template('test.html',post={'www.google.com':'8.8.8.8'},lon=77.102,lat=28.704)
     return render_template("dnslookup.html", title="Login", form=form)
 
+@app.route("/Revdnslookup", methods=["GET", "POST"])
+def Revdnslookup():
+    form = RevForm()
+    if form.validate_on_submit():
+        return render_template('test.html',post={'www.google.com':'8.8.8.8'},lon=77.102,lat=28.704)
+    return render_template("revlookup.html", title="Login", form=form)
+
+@app.route('/test')
+def test():
+    return render_template('test.html',post={'www.google.com':'8.8.8.8'},lon=77.102,lat=28.704)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, debug=True)
+    app.run(host='0.0.0.0', threaded=True,debug=True)
--- DNS_Project/form.py
+++ DNS_Project/form.py
@@ -2,12 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
-
 class DNSForm(FlaskForm):
-    myChoices = [["A", "A"], ["AAAA", "AAAA"], [
-        "MX", "MX"], ["CA", "CA"], ["TXT", "TXT"]]
-    domain_name = StringField('Domain name', validators=[DataRequired()])
-    type = SelectField("Record Type", choices=myChoices,
-                       validators=[DataRequired()])
+    myChoices =  [["A","A"],["AAAA","AAAA"],["MX","MX"],["CA","CA"],["TXT","TXT"]]
+    domain_name = StringField('Domain name',validators=[DataRequired()])
+    type= SelectField("Record Type", choices = myChoices, validators = [DataRequired()])
     default_dns = StringField('Default DNS', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class RevForm(FlaskForm):
+    domain_name = StringField('Domain name',validators=[DataRequired()])
     submit = SubmitField('Submit')
