5c8f12f749eec1e5e9de3ad2a290f8ab547fedb2
dak-x <2018UCS0068@iitjammu.ac.in>
2020-10-05 21:42:40 +0530

    Added functionality for getting records ... Not yet complete

--- DNS_Project/form.py
+++ DNS_Project/form.py
@@ -1,14 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+import dns.rdatatype
+
 
 class DNSForm(FlaskForm):
-    myChoices =  [["A","A"],["AAAA","AAAA"],["MX","MX"],["CA","CA"],["TXT","TXT"]]
-    domain_name = StringField('Domain name',validators=[DataRequired()])
-    type= SelectField("Record Type", choices = myChoices, validators = [DataRequired()])
+    myChoices = [
+        [dns.rdatatype.A, "A"],
+        [dns.rdatatype.AAAA, "AAAA"],
+        [dns.rdatatype.CNAME, "CNAME"],
+        [dns.rdatatype.MX, "MX"],
+        [dns.rdatatype.NS, "NS"],
+        [dns.rdatatype.PTR, "PTR"],
+        [dns.rdatatype.SRV, "SRV"],
+        [dns.rdatatype.SOA, "SOA"],
+        [dns.rdatatype.TXT, "TXT"],
+        [dns.rdatatype.CAA, "CAA"]
+    ]
+    domain_name = StringField('Domain name', validators=[DataRequired()])
+    type = SelectField("Record Type", choices=myChoices,
+                       validators=[DataRequired()])
     default_dns = StringField('Default DNS', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 class RevForm(FlaskForm):
-    domain_name = StringField('Domain name',validators=[DataRequired()])
+    domain_name = StringField('Domain name', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- DNS_Project/trial2.py
+++ DNS_Project/trial2.py
@@ -1,6 +1,47 @@
-import socket
-an_address = "8.8.8.8"
+import dns.message
+import dns.rdatatype
+import dns.query
 
-domain_name = socket.gethostbyaddr(an_address)
+def get_records(domain_name: str, record_type: dns.rdatatype.RdataType, dns_addr: str):
 
-print(domain_name)
+    Qry_List = []
+    if(record_type == dns.rdatatype.RdataType.ANY):
+        Qry_List = [dns.rdatatype.A, dns.rdatatype.AAAA, dns.rdatatype.CNAME, dns.rdatatype.MX, dns.rdatatype.NS,
+                    dns.rdatatype.PTR, dns.rdatatype.SRV, dns.rdatatype.SOA, dns.rdatatype.TXT, dns.rdatatype.CAA]
+    else:
+        Qry_List = [record_type]
+
+    response_list = []
+
+    for qry in Qry_List:
+        k = dns.message.make_query(domain_name, qry)
+        msg: dns.message.Message = dns.query.tcp(k, where=dns_addr)
+        print(msg.answer)
+        # msg.find_rrset(msg.answer,domain_name,)
+        # print(msg.to_text())
+        # continue
+
+        lines = str(msg).split('\n')
+        qry_response = []
+        flag = False
+        for line in lines:
+            if( line.count("AUTHORITY") == 1):
+                flag = False
+                continue
+            if(line.count("ANSWER") == 1):
+                flag = True
+                continue
+            if(flag):
+                qry_response.append(line)
+        # u = qry_response.split(" ")
+        response_list.append((qry.name,u))
+
+    return response_list
+
+temp = get_records("iitjammu.ac.in", dns.rdatatype.ANY, "8.8.8.8")
+for x in temp:
+    print(x)
+#
+# an_address = "8.8.8.8"
+# domain_name = socket.gethostbyaddr(an_address)
+# print(domain_name)
